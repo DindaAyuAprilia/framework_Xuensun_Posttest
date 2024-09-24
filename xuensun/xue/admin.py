ec2ada3ecc63882c5ad4673fb336efcd9bfe954b
DindaAyuAprilia <ajadinda975@gmail.com>
2024-09-24 08:19:10 +0800

    posttest2 perbaikin

--- xuensun/xue/admin.py
+++ xuensun/xue/admin.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 
 # Register your models here.
-=======
-from django.contrib import admin
-
-# Register your models here.
->>>>>>> 4ba49aabbb0e6db88f230316516cc65a0e4639a3
